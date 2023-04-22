0e22a618a9166c2b2bc17e3480224cfcc5aafca8
Danush Athithya <danushathithya.24cs@licet.ac.in>
2023-04-23 02:30:27 +0530

    first staging

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import logo from "./logo1.png";
+import Background from "./background";
+import Section0 from "./section0";
+import Section1 from "./Section1";
+import Section2 from "./section2";
+import Form from "./form";
+import Image from "./image";
+import Footer from "./footer";
+import { scroller } from "react-scroll";
+import Card from './card';
+import Team from "./Team";
+
 
 function App() {
+  const scrollTo = (id) => {
+    scroller.scrollTo(id, {
+      duration: 800,
+      delay: -2,
+      smooth: "easeInOutQuart",
+    });
+  };
+  const currentPath = window.location.pathname;
+  if (currentPath === "/team") {
+    return <Team data-testid="team-component" />;
+  } else {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div style={{}} className="">
+      <nav
+        className="flex border-b-2  border-sky-400 items-center justify-between flex-wrap p-2 navbar bg-gradient-to-r from-zinc-900  via-indigo-950 to-zinc-900"
+        id="nav1"
+      >
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="#"
+          className="flex items-center flex-shrink-0 mr-6 justify-start col-2 col-sm-2 col-md-2 col-lg-2 col-xl-2 col-xxl-2"
         >
-          Learn React
+          <img src={logo} alt="Logo" className="logo-image mx-auto b-block" style={{ }} />
+
         </a>
-      </header>
+        <div className="flex-grow ">
+          <ul className="flex justify-end ">
+            <li className="pr-9 pl-5 pt-4 pb-4">
+              <a
+                href="#card"
+                className="hov under size"
+                onClick={() => scrollTo("card")}
+              >
+                Events
+              </a>
+            </li>
+            <li className=" pr-9 pt-4 pb-4">
+              <a
+                href="/team"
+                className="hov under size"
+              
+              >
+                Team
+              </a>
+            </li>
+            <li className=" pr-9 pt-4 pb-4">
+              <a
+                href="#"
+                className="hov under size"
+                onClick={() => scrollTo("event")}
+              >
+                Gallery
+              </a>
+            </li>
+            <li className="pr-2 pt-4 pb-4">
+              <a
+                href="#form"
+                className="hov under size"
+                onClick={() => scrollTo("form")}
+              >
+                Contact
+              </a>
+            </li>
+            <li className="  pt-4 pb-4">
+              <a
+                href="#form"
+                className="hov under size"
+                onClick={() => scrollTo("form")}
+              >
+                
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Background/>
+      <Section0 />
+      <Section1 />
+      <Section2 />
+      <Image />
+      <Card/>
+      
+      <Form />
+
+      <Footer />
     </div>
   );
+  }
 }
+
+
 
 export default App;
---
+++ src/card.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "./card.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import pic from "./bgpic.png";
+
+const Card = () => {
+  return (
+    <>
+      <div style={{ fontFamily:'Montserrat'}} className="bg-red-100" id="card"> 
+        <div
+          className="rounded-xl"
+          style={{  }}
+        >
+          <div
+            className="col-sm-4 pb-5 pt-5 mx-auto d-block col-xl-3 col-xxl-3 col-4  col-md-4 col-lg-3 "
+            
+          >
+            <div
+              className="card rounded-lg border-2 border-zinc-950 hovv bg-gradient-to-b from-zinc-800 via-indigo-700 to-zinc-800"
+              style={{ }}
+            >
+             
+              <img
+                src={pic}
+                style={{ objectFit: "cover" }}
+                className=" border border-zinc-950  mx-auto d-block "
+                alt="..."
+              />
+              <div className="card-body bg-gradient-to-b text-cyan-50 text-teal-300">
+                <h5 className="card-title">
+                <b style={{fontSize:'120%'}}>Swimming</b>
+                </h5>
+                <p className="card-text pb-3">
+                  AQUA Festival will bring together swimming, everyone is
+                  invited to join in fun swimming races, Team TRI2CHAMP,
+                  Welcoming Everyone we cordially invite you to our very biggest
+                  swimming event. AQUAFEST - SWIMATHON & AQUATHLON Chennai race
+                  venue allows us to deliver a first-class event experience to
+                  wrap up the 2023 season. 
+                </p>
+                <a
+                  href="https://www.townscript.com/e/Aquafest-140333"
+                  className="btn btn-primary bg-gradient-to-b from-emerald-600 to-indigo-900 hover:from:emerald-400 hover:to-indigo-700"
+                  style={{}}
+                >
+                  Register
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
