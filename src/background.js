19a2c30a4aea192ba0a94ab4c548d5423d42aacb
Tri2ChampOfficial <131596809+Tri2ChampOfficial@users.noreply.github.com>
2023-06-17 11:42:30 +0530

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,9 +1,10 @@
 import React from "react";
+import picc from "./emptybackground.jpeg";
 
 const Background = () => {
   return (
     <div className="">
-      <img src={} useMap="map" class="object-fit-cover border rounded" alt="No Events Yet" />
+      <img src={picc} useMap="map" class="object-fit-cover border rounded" alt="No Events Yet" />
       <map name="map">
         <area shape="rect" alt="link" coords="0,0,1000,1000" href="https://www.townscript.com/e/Aquafest-140333" />
       </map>
